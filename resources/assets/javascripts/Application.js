d8a0c7a6650998afbac501328927923078e32f19
dnnsys <as@rsdigital.ru>
2017-12-11 23:40:43 +0300

    project init

--- resources/assets/javascripts/Application.js
+++ resources/assets/javascripts/Application.js
@@ -1,10 +1,20 @@
 //VENDORS
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
+require('jquery-nice-select');
 
 class Application{
     constructor(){
-        console.log('application start');
+        this._langSwitcher();
+        this._searchComponent();
+    }
+
+    _langSwitcher(){
+        $('select.lang-switch-select').niceSelect();
+    }
+
+    _searchComponent(){
+        $('select.search-select').niceSelect();
     }
 }
 
